1a841b9e0087f18f37c174c61316482ed6422be9
VyrTtZ <tarkagustaw@gmail.com>
2026-09-24 13:41:17 +0100

    added some extra things to manage startup macvlan setup

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -fsSL https://docker.com -o get-docker.sh
 sudo sh get-docker.sh
-
+sudo yay install nmap
 sudo systemctl enable docker
 sudo systemctl start docker
 
@@ -13,3 +13,20 @@ do
 	ip addr show scope global | grep -E  '[0-9]{1,3}\.[0-9]{1-3}\.[0-9]{1-3}\.[0-9]{1,3}/[0-9]{1-2}'
 #gets the name of u NIC
 ip addr show scope global | grep -E 'wlp.*\:' 2>/dev/null
+
+#ip -o addr | grep "inet\ " | awk '{print $4}'
+#prints both loopback and normal ip
+
+#ip -o addr | grep "wl.*" | grep "inet\ " 2>/dev/null | awk '{print $4}'
+#prints just the normal IP YIPPEEEE
+
+
+ip -o addr | grep "wl.*" | grep "inet\ " 2>/dev/null | awk '{print $2}'
+#prints just the wireless NIC name YIPEEEEEE
+
+ip -o addr | grep "wl.*" | grep "inet\ " 2>/dev/null | awk '{print $6}'
+#prints the default gateway
+
+
+nmap -sn -T4 -n -PR $(ip -o addr | grep "wl" | awk '/inet / {print $4}')
+#prints out the hosts currently on the network, quick scan, no dns lookups, aggresive timing templates
